53dcf86f332a0e2116fa6120df2dd277181af415
DDDrop <wylroro.dev@gmail.com>
2017-10-06 08:47:16 +0900

    修改

--- keyboards/ergodox_ez/keymaps/dddrop_base/keymap.c
+++ keyboards/ergodox_ez/keymaps/dddrop_base/keymap.c
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LYR_00] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_PIPE,    KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_EQL,     KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   ,
+        KC_EQL,     KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_LGUI,    KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,    CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(LYR_01,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        LT(LYR_01,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LALT ,KC_LGUI,
                                               ALT_T(KC_APP),  TG(LYR_01),
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_DELT,
